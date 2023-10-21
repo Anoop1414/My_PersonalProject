4d30f0dbb420e0b94f196127e1c2d162b7646080
ANOOP KUMAR <148521901+Anoop1414@users.noreply.github.com>
2023-10-21 11:25:27 +0530

    Add files via upload

---
+++ LoginPageM.java
@@ -0,0 +1,41 @@
+package MultithreadP;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPageM {
+
+
+		
+		WebDriver driver; 
+		//constructor
+		LoginPageM(WebDriver d){
+			driver =d;
+		}
+		By username = By.id("user-name");
+		
+		//WebElement username = driver.findElement(By.id("user-name"));
+		By password = By.id("password");
+		By login_btn = By.name("login-button");
+		
+		public void enterusername(String uname)
+		{
+			driver.findElement(username).sendKeys(uname);
+		}
+		public void enterpswd(String pswd)
+		{
+			driver.findElement(password).sendKeys(pswd);
+		}
+		
+		public void clickBtn()
+		{
+			driver.findElement(login_btn).click();
+	
+	
+	
+		}	
+	
+	
+	
+	
+}
